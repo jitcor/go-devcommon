c4cc861cfb38191006d9a671bf212daa9e574fca
shlu <shilulu@forensix.cn>
2022-01-05 16:30:42 +0800

    dcbytes.go

--- dcbytes/dcbytes.go
+++ dcbytes/dcbytes.go
@@ -1,6 +1,10 @@
 package dcbytes
 
-import "github.com/Humenger/go-devcommon"
+import (
+	"errors"
+	"github.com/Humenger/go-devcommon"
+	"io"
+)
 
 func BytesCopy( dst []byte, dstOffset int,src []byte, srcOffset int, length int) {
 	minLength:=devcommon.Min(length,devcommon.Min(len(src)-srcOffset,len(dst)-dstOffset))
@@ -8,4 +12,31 @@ func BytesCopy( dst []byte, dstOffset int,src []byte, srcOffset int, length int)
 		copy(dst[dstOffset:dstOffset+minLength],src[srcOffset:srcOffset+minLength])
 	}
 
+}
+func ReaderCopy(reader io.Reader, out []byte) error {
+	outBuf:=make([]byte,0)
+	l:= len(out)
+	buf:=make([]byte,devcommon.Min(l,1024))
+	if n,err:=reader.Read(buf);err!=nil&&err!=io.EOF{
+		return err
+	}else {
+		for n > 0 {
+			outBuf=append(outBuf,buf[0:n]...)
+			l-=n
+			if l==0{
+				break
+			}
+			if l<1024{
+				buf=make([]byte,l)
+			}
+			if n,err=reader.Read(buf);err!=nil&&err!=io.EOF{
+				return err
+			}
+		}
+		if len(outBuf)> len(out){
+			return errors.New("ReaderCopy is error")
+		}
+		BytesCopy(out,0,outBuf,0, len(outBuf))
+		return nil
+	}
 }
--- dcbytes/dcbytes_test.go
+++ dcbytes/dcbytes_test.go
@@ -1,6 +1,7 @@
 package dcbytes
 
 import (
+	"bytes"
 	"log"
 	"testing"
 )
@@ -9,5 +10,13 @@ func TestBytesCopy(t *testing.T) {
 	dst:=[]byte{0,1,2,3,4,5,6,7,8,9}
 	src:=[]byte{10,11,12,13,14,15,16}
 	BytesCopy(dst,0,src,5,4)
+	log.Println("dst:",dst)
+}
+func TestReaderCopy(t *testing.T) {
+	src:=[]byte{0,1,2,3,4,5,6,7,8,9}
+	dst:=make([]byte,20)
+	if err:=ReaderCopy(bytes.NewBuffer(src),dst);err!=nil{
+		t.Error(err)
+	}
 	log.Println("dst:",dst)
 }
